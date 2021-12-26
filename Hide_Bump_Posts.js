9ffbc6918f988f35147494e770dd0c0b81f7ce15
Sango <74567441+moistp1ckle@users.noreply.github.com>
2021-12-26 05:22:20 -0600

    Create Hide_Bump_Posts.js

---
+++ Hide_Bump_Posts.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Hide Bumps
+// @namespace    https://leaked.wiki
+// @version      0.1
+// @description  Auto hide bump messages.
+// @author       https://cracked.io/Sango
+// @match        *://cracked.io/Thread-*
+// @icon         https://www.google.com/s2/favicons?domain=cracked.io
+// @grant        none
+// @require      https://static.cracked.io/jscripts/jquery.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function hide() {
+        $("#posts > div").each(function (i) {
+            if (~$(this).find(".post_body").text().indexOf("This is a bump") || ~$(this).find(".post_body").text().indexOf("System message: this post was autobumped")) {
+                var pid = $(this).addClass("post-banned");
+                var $bit = $(this).find(".post-banned-bit");
+                $bit.html($bit.html().replace("This post is by a banned member", "This post is a bump by"));
+            }
+        });
+    }
+    hide();
+})();
